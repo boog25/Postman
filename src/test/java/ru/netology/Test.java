5077fb8b9b59dc0ac997e3f7dce0eae65f9fdd4a
boog25 <voroshilov.d.v123@gmail.com>
2021-02-28 15:06:35 +0500

    Initial commit

---
+++ src/test/java/ru/netology/Test.java
@@ -0,0 +1,18 @@
+package ru.netology;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Test {
+    @org.junit.jupiter.api.Test
+    void shouldPass() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
